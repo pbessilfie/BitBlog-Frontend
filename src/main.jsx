8839e200707b0457c9a7d2cb5ed59ab259f6fbe6
pbessilfie <pbessilfie001@st.ug.edu.gh>
2025-04-08 17:44:45 +0000

    refactor: reorder import statements for better organization

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import Home from "./pages/Home";
 import Blogs from "./pages/Blogs";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import SingleBlog from "./pages/SingleBlog";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const router = createBrowserRouter([
   {
